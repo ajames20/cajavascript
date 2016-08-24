972b2aac8b8b74ec9844cd6e05afcbf990f62f48
Andrew James <andrew.james@my.com>
2016-08-24 13:22:47 +0300

    update

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -1,2 +1,13 @@
 
-$("#main").append(["Andrew"]);
+//$("#main").append(["Andrew"]);
+
+
+
+var awesomeThoughts = " I am Andrew and I am Awesome!"
+
+var funToughts = awesomeThoughts.replace("Awesome", "Fun")
+
+console.log(awesomeThoughts);
+console.log(funToughts);
+
+$("#main").append(funToughts);
